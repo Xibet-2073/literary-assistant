06151f5b0d2419a95f0ed2bb437bbcea4e187eb5
Xibet-2073 <Xibet-2073>
2024-10-03 22:43:14 +0200

    Project setup

---
+++ src/main/java/com/xibet/literary_assistant/LiteraryAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.xibet.literary_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraryAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraryAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xibet/literary_assistant/LiteraryAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xibet.literary_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraryAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
